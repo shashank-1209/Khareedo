0791aa22d0050abd22786a7dd373b0e1035da2c8
Shashank singh <shanku1209@gmail.com>
2021-04-07 16:36:42 +0530

    make it better

---
+++ backend/config.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+  PORT: process.env.PORT || 5000,
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/bazzar',
+  JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
+  accessKeyId: process.env.accessKeyId || 'accessKeyId',
+  secretAccessKey: process.env.secretAccessKey || 'secretAccessKey',
+
+};

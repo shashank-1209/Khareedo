83a506ae72c023a507fcde5d401458e81dd99df4
Shashank singh <shanku1209@gmail.com>
2021-04-08 12:26:21 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -32,13 +32,12 @@ app.get('*', (req, res) => {
 });
 
 //heroku
-if(config.PORT=="production"){
-  app.use(express.static('./frontend/build'));
-  const path=require('path');
-  app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
-  })
-}
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 
 app.listen(config.PORT, () => {
   console.log('Server started at http://localhost:5000');
